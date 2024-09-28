effe64d6c558f8b3c9598228a3077b7ba49b84a2
rdanilowicz <rjdanilowicz@gmail.com>
2024-09-28 19:07:51 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
