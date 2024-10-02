72592941367a40cd26077a6a6c4184d6273d45b4
rdanilowicz <rjdanilowicz@gmail.com>
2024-10-01 20:56:08 -0400

    Fixed some of the views and added some DB logic.

--- Controllers/KBController.cs
+++ Controllers/KBController.cs
@@ -1,6 +1,7 @@
 using KnowIT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System.Diagnostics;
 
 namespace KnowIT.Controllers
@@ -45,6 +46,87 @@ namespace KnowIT.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View (article);
+        }
+
+        // GET: Knowledge/Edit
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // POST: Knowledge/Edit
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content")] Article article)
+        {
+            if (id != article.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(article);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleExists(article.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: Knowledge/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var article = await _context.articles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return View(article);
+        }
+
+        //POST: Knowledge/Delete
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var article = await _context.articles.FindAsync(id);
+            _context.articles.Remove(article);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.articles.Any(m => m.Id == id);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/Article.cs
+++ Models/Article.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace KnowIT.Models
 {
     public class Article
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
         public DateTime DateCreated {  get; set; }
         public int CategoryID { get; set; }
