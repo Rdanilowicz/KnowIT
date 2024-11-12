60db22de7bac71d3bb6a541d3bf3bb06c867dd0e
rdanilowicz <rjdanilowicz@gmail.com>
2024-11-11 21:05:05 -0500

    Added a homepage!!!!!!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using KnowIT.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowIT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly KnowledgeDbContext _context;
+
+        public HomeController(KnowledgeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var featuredCategories = await _context.Categories.Take(3).ToListAsync(); // Example for showing 3 popular categories
+            var model = new HomeViewModel
+            {
+                FeaturedCategories = featuredCategories
+            };
+
+            return View(model);
+        }
+        public class HomeViewModel
+        {
+            public IEnumerable<Category> FeaturedCategories { get; set; }
+        }
+
+    }
+}
